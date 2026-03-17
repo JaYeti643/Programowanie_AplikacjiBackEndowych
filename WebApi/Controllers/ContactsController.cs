822172026d0ec36ad41e6488cbb10f2d8886d19d
Jakub Piechowicz <jakub.piechowicz@microsoft.wsei.edu.pl>
2026-03-17 12:26:58 +0100

    lab3

---
+++ WebApi/Controllers/ContactsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using AppCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("/api/contacts")]
+public class ContactsController(IPersonService service) : ControllerBase
+{
+    [HttpGet("persons")]
+    public async Task<IActionResult> GetAllPersons(int page, int size)
+    {
+        return Ok(await service.FindAllPeoplePaged(page, size));
+    }
+}
